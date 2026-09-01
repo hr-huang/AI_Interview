c6e8c83206cd47044aa8fb3ed8fc6167feae2cd2
黄海睿 <huang6391528@gmail.com>
2026-09-01 22:49:19 +0800

    feat: route plan review through enterprise progress shell

--- web/src/app/router.tsx
+++ web/src/app/router.tsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { AnalyzingPage } from '../features/assessments/AnalyzingPage'
 import { NewAssessmentPage } from '../features/assessments/NewAssessmentPage'
 import { InterviewPage } from '../features/interview/InterviewPage'
-import { PlanReviewPage } from '../features/plans/PlanReviewPage'
+import { EnterprisePlanReviewPage } from '../features/plans/EnterprisePlanReviewPage'
 import { DemoReportPage } from '../features/report/DemoReportPage'
 import { ReportPage } from '../features/report/ReportPage'
 
@@ -11,7 +11,7 @@ export function AppRouter() {
     <Routes>
       <Route path="/assessments/new" element={<NewAssessmentPage />} />
       <Route path="/assessments/:assessmentId/analyzing" element={<AnalyzingPage />} />
-      <Route path="/assessments/:assessmentId/plan" element={<PlanReviewPage />} />
+      <Route path="/assessments/:assessmentId/plan" element={<EnterprisePlanReviewPage />} />
       <Route path="/assessments/:assessmentId/report" element={<ReportPage />} />
       <Route path="/interviews/:candidateToken" element={<InterviewPage />} />
       <Route path="/demo/assessment" element={<DemoReportPage />} />
